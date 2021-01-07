b38b38b1fb2ff3ae19d770a2b9f0555e7bdb5219
Izzaxx <luisangelizaguirrehernandez@gmail.com>
2021-01-06 22:31:34 -0700

    [ADD] Get para visualizar tipo de habitacion con el id

--- server/src/controllers/Interno/tipoHabitacion.controller.js
+++ server/src/controllers/Interno/tipoHabitacion.controller.js
@@ -3,20 +3,25 @@ const tipoHabitacionCtrl = {};
 const TipoHabitacion = require('../../models/TipoHabitacion');
 
 //Find all type room
-tipoHabitacionCtrl.getTipoHabitacion = async (req, res) => {
+tipoHabitacionCtrl.getTipoHabitacion = async(req, res) => {
     const tipohabitacion = await TipoHabitacion.find();
     res.json(tipohabitacion);
 }
 
 //Data save type room
-tipoHabitacionCtrl.saveTipoHabitacion = async (req, res) => {
+tipoHabitacionCtrl.saveTipoHabitacion = async(req, res) => {
     const { nombre, cantidad } = req.body;
     const newTipoHabitacion = new TipoHabitacion({
-        nombre, 
+        nombre,
         cantidad
     });
     await newTipoHabitacion.save();
     res.status(200).json({ message: 'type room saved' });
+}
+
+tipoHabitacionCtrl.getTipoHabitacionId = async(req, res) => {
+    const tipoHabitacion = await TipoHabitacion.findById(req.params.id);
+    res.json(tipoHabitacion);
 }
 
 module.exports = tipoHabitacionCtrl;
--- server/src/routes/Interno/tipoHabitacion.js
+++ server/src/routes/Interno/tipoHabitacion.js
@@ -1,13 +1,17 @@
 const { Router } = require('express');
 const router = Router();
 
-const { 
-    getTipoHabitacion, 
-    saveTipoHabitacion 
+const {
+    getTipoHabitacion,
+    saveTipoHabitacion,
+    getTipoHabitacionId
 } = require('../../controllers/Interno/tipoHabitacion.controller');
 
 router.route('/')
     .get(getTipoHabitacion)
     .post(saveTipoHabitacion)
+
+router.route('/:id')
+    .get(getTipoHabitacionId)
 
 module.exports = router
