e098b943b5743aedae77fd575e43975c08f8f3a7
Izzaxx <luisangelizaguirrehernandez@gmail.com>
2021-01-21 00:35:44 -0700

    [UPDATE] Estatus para cuartos ocupados para consumos

--- server/src/controllers/Externo/cuartoOcupado.controller.js
+++ server/src/controllers/Externo/cuartoOcupado.controller.js
@@ -7,7 +7,7 @@ cuartoOcupadoCtrl.getRoomOccupied = async(req, res) => {
     if (room.disponible === false) {
         res.status(200).json({ message: `Este cliente es huesped del hotel, de la habitacion ${room.noCuarto}`, status: 200 })
     } else {
-        res.status(400).json({ message: "Este cliente no es huesped del hotel", status: 200})
+        res.status(400).json({ message: "Este cliente no es huesped del hotel", status: 400})
     }
 }
 
